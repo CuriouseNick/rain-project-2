d919f19b501cb88f83ad483e3fe6f2fb45593968
RRaethke <reece.raethke@outlook.com>
2018-07-14 14:54:53 -0500

    added login and user auth

--- server.js
+++ server.js
@@ -3,10 +3,12 @@ var express = require("express");
 var bodyParser = require("body-parser");
 // ******************************************************************************
 var app = express();
-var PORT = process.env.PORT || 8080;
+var passport = require('passport')
+var session = require('express-session')
+var env = require('dotenv').load()
+var exphbs = require('express-handlebars')
 
-// Requiring our models for syncing
-var db = require("./models");
+var PORT = process.env.PORT || 8080;
 
 // Sets up the Express app to handle data parsing
 
@@ -18,23 +20,56 @@ app.use(bodyParser.json());
 // Static directory
 app.use(express.static("public"));
 
+// For Passport
+app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+
+//For Handlebars
+app.set('views', './auth/views')
+app.engine('hbs', exphbs({ extname: '.hbs' }));
+app.set('view engine', '.hbs');
+
+app.get('/', function (req, res) {
+  res.send('Welcome to Passport with Sequelize');
+});
+
+//Models
+var models = require("./models");
+
 // Routes
 // =============================================================
 
 require("./routes/apiRoutes.js")(app);
+var authRoute = require('./routes/auth.js')(app, passport);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync().then(function() {
-  app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
-  });
+
+// console.log('this is our models!!!!!', models);
+//load passport strategies
+require('./config/passport/passport.js')(passport, models.user);
+
+
+//Sync Database
+models.sequelize.sync().then(function () {
+  console.log('Nice! Database looks fine')
+
+}).catch(function (err) {
+  console.log(err, "Something went wrong with the Database Update!")
 });
+
+
+
+app.listen(8080, function (err) {
+  if (!err)
+    console.log("Site is live"); else console.log(err)
+
+});
+
 
 
 // johnny five photo resistor
 var five = require("johnny-five"),
-board, photoresistor, temperature, led;
+  board, photoresistor, temperature, led;
 
 var light;
 var temperature;
@@ -42,7 +77,7 @@ var locTemp;
 
 board = new five.Board();
 
-board.on("ready", function() {
+board.on("ready", function () {
 
   // Create a new `photoresistor` hardware instance.
   photoresistor = new five.Sensor({
@@ -67,19 +102,19 @@ board.on("ready", function() {
   });
 
   // "data" get the current reading from the photoresistor
-  photoresistor.on("data", function() {
+  photoresistor.on("data", function () {
     light = this.value;
     console.log(light);
   });
 
-  temperature.on("data", function(){
+  temperature.on("data", function () {
     locTemp = this.fahrenheit;
     console.log(locTemp);
   });
 });
 
-app.get("/api/temp", function(req, res){
-  res.json({"temperature": locTemp});
+app.get("/api/temp", function (req, res) {
+  res.json({ "temperature": locTemp });
 });
 
 app.get("/api/light")
